3effd0180fc5f82dce4ccde406b3da876b6f11d6
aasitha <aasithasanagapalli@gmail.com>
2023-02-21 15:27:22 +0530

    6th

--- test.js
+++ test.js
@@ -1,14 +1,1 @@
-const mongoose = require("mongoose");
-mongoose.set("strictQuery",false);
-mongoose.connect("mongodb://127.0.0.1:27017/assignment",{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-},(err)=>{
-    if(err){
-        console.log("error "+err);
-        res.send(err.toString());
-    }else{
-        console.log("successfully connected");
-        //res.send("successfully connected");
-    }
-});
+start grou
