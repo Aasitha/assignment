432b9a009ad01df104ddf8d633b1101e8ce99d95
aasitha <aasithasanagapalli@gmail.com>
2023-03-01 16:31:21 +0530

    18th

--- client2.js
+++
@@ -1,8 +0,0 @@
-var data={
-    dommand:2,
-    msg:"aasitha, rishiya, paavallika,"
-}
-var info=JSON.stringify(data);
-var data2=JSON.parse(info)
-console.log(data2.msg.split(", "));
-console.log(typeof(data2.msg));
